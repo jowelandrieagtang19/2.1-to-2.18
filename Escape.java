5a813400120474b7142fb36c151632bc68d03330
Andrie <jowelandrie.a.agtang@isu.edu.ph>
2026-09-05 21:55:24 +0800

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,16 @@
+//Jowel Andrie Agtang
+//BSIT NETSEC 1-1
+package escape;
+public class Escape {
+
+    public static void main(String[] args) {
+        
+       System.out.println("Name : First Name        Last Name");
+       System.out.println("Course/Year/Section :"); 
+       System.out.println("Student Number:           Number: ");
+       System.out.println("Jowel Andrie             Agtang"); 
+       System.out.println("BSIT NETSEC/1/1-1");
+       System.out.println("26-160                   09511787871"); 
+    }
+    
+}
---
+++ Number.java
@@ -0,0 +1,17 @@
+//Jowel Andrie Agtang
+//BSIT NETSEC 1-1
+package number;
+
+public class Number {
+
+    
+    public static void main(String[] args) {
+      System.out.println("number\tsquare\tcube");
+      System.out.println("0\t0\t0") ;
+      System.out.println("1\t1\t1") ;  
+      System.out.println("2\t4\t8") ;  
+      System.out.println("3\t9\t27") ;  
+      
+    }
+    
+}
